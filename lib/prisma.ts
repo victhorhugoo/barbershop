7d46cbd798b36fd863d018dfcf71d92940ffd93c
victhorhugoo <victhorhugo.dev@gmail.com>
2025-12-09 18:44:16 -0300

    chore: fix prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
 // lib/prisma.ts
 
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "../generated/prisma";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const connectionString = `${process.env.DATABASE_URL}`;
