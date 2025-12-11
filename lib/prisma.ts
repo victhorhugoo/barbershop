4eb8b510739c3e952791e805a7b2fc38e4482b5d
victhorhugoo <victhorhugo.dev@gmail.com>
2025-12-10 21:13:55 -0300

    chore: fix seed.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
 // lib/prisma.ts
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const connectionString = `${process.env.DATABASE_URL}`;
